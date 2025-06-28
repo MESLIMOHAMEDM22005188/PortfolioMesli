3ccd46f75c39886799098a14695f95c3a8a3dc19
mesli <mohamed.mesli@etu.univ-amu.fr>
2025-06-28 17:34:03 +0200

    add requirements

--- monsite/manage.py
+++ monsite/manage.py
@@ -2,8 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-
+print("BASE DIR:", os.getcwd())
+print("FILES:", os.listdir())
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monsite.settings')
