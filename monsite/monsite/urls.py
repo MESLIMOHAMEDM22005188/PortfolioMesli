89844a2cce387e0faf026c486d6e16c707549c2c
Mesli Mohamed <mmesli@cerege.fr>
2026-06-21 23:16:02 +0200

    refactor: improve root URL configuration structure

--- monsite/monsite/urls.py
+++ monsite/monsite/urls.py
@@ -1,18 +1,15 @@
-# monsite/urls.py  (le routeur principal du projet)
+"""
+Root URL configuration.
+
+Routes requests to the appropriate application.
+"""
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+
 
 urlpatterns = [
-    # 1) La route /admin/ pour accéder à l’admin Django
-    path('admin/', admin.site.urls),
-
-    # 2) Tout ce qui commence par /tickets/ => on bascule sur tickets/urls.py
-    #    Exemple : /tickets/        ➜ tickets/urls.py, route vide ''
-    #              /tickets/reset/  ➜ tickets/urls.py, route 'reset/'
-    path('tickets/', include('tickets.urls', namespace='tickets')),
-
-    # 3) Tout le reste (la racine "/", etc.) => on bascule sur portfolio/urls.py
-    #    Exemple : /       ➜ portfolio/urls.py, route vide ''
-    #              /primaire/ ➜ portfolio/urls.py, route 'primaire/'
-    path('', include('portfolio.urls', namespace='portfolio')),
+    path("admin/", admin.site.urls),
+    path("tickets/", include("tickets.urls", namespace="tickets")),
+    path("", include("portfolio.urls", namespace="portfolio")),
 ]
